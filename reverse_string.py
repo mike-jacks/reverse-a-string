28e69651bc416c6f014e657f5fc5118960948437
Mike Jacks <mjacks4816@xmail.dixietech.edu>
2024-04-15 08:16:45 -0600

    init commit

---
+++ reverse_string.py
@@ -0,0 +1,5 @@
+# Make a function that reverses a string
+
+def reverse_string(s: str) -> str:
+    return s[::-1]
+
---
+++ test_reverse_string.py
@@ -0,0 +1,11 @@
+import pytest
+from pytest import fixture
+
+from reverse_string import reverse_string
+
+@fixture
+def string():
+    return "Hello"
+
+def test_reverse_string(string):
+    assert reverse_string(string) == "olleH"
